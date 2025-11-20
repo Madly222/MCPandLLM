82df402bb84927d6cc7ec5f0d632aa2e3da52fde
Stas <132693808+Madly222@users.noreply.github.com>
2025-11-20 16:34:28 +0200

    Mda

--- vector_store/__init__.py
+++ vector_store/__init__.py
@@ -3,6 +3,6 @@ Vector Store пакет с абстракцией для легкой замен
 """
 
 from vector_store.interface import VectorStoreInterface
-from vector_store.weaviate_store import WeaviateStore, vector_store
+from vector_store.wv_store import WeaviateStore, vector_store
 
 __all__ = ['VectorStoreInterface', 'WeaviateStore', 'vector_store']
